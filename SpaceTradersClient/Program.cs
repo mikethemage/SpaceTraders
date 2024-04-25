5e96792880287ed0591b902bcb06068cef0a6567
Mike Dunn <mdunn147@gmail.com>
2024-04-25 18:12:52 +0100

    Add new project for testing

---
+++ SpaceTradersClient/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceTradersClient;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        //Initialisation:
+        //Get API key from storage
+        //If we have an API key, attempt login
+        //If login failed - delete API key
+        //If no API key, or login failed - register
+        //If registration failed - exit
+
+        //We now should be logged on
+    }
+}
